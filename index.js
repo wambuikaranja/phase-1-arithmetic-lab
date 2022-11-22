ab6ce0554a7ef41c716d904cf9486a542baa7e73
wambuikaranja <karanjawambui492@gmail.com>
2022-11-22 11:05:16 +0300

    Lab Completed

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+//two number giving a product of 62
+const num1 = 31
+const num2 = 2
 
-// Write your code here
+const multiply = num1 * num2
+
+
+//random
+
+const random  = Number.isInteger(Math.random() * 10) + 1
+
+//mod
+
+const num3 = 44
+const num4 = 10
+
+const mod = num3 % num4
+
+//max
+let max = Math.max(15,16,17,18,19,20)
+
+max
